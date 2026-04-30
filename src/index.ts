00f3691683f8102a7ce2843d265f7ed82f42d6ad
da4aaa <da4aaa@gmail.com>
2026-04-30 16:52:54 -0500

    feat: barrel exports and vercel config

---
+++ src/index.ts
@@ -0,0 +1,7 @@
+// src/index.ts
+export * from './components/Avatar'
+export * from './components/Badge'
+export * from './components/Tag'
+export * from './components/Button'
+export * from './components/Dropdown'
+export * from './components/QuickReplyChip'
